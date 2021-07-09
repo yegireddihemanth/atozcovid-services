b5f178c9518c962f2edbfd2202756b4f02e1b5de
chintalapudipraneeth <47571572+chintalapudipraneeth@users.noreply.github.com>
2021-07-09 19:17:37 +0530

    Add files via upload

---
+++ hospitaldb/db.php
@@ -0,0 +1,10 @@
+<?php
+    // Enter your host name, database username, password, and database name.
+    // If you have not set database password on localhost then set empty.
+ //   $con = mysqli_connect("localhost","root","","atozcovidservices");
+    // Check connection
+    $con=mysqli_connect('remotemysql.com','AJtedatBu3','ayYeXSJSOx','AJtedatBu3');
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+?>
