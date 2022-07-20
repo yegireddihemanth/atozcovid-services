60e227ec33f1e4dd1d34d3cf1c268a38fdb67378
yegireddihemanth <hemanthkumar.yegireddy@jobiak.com>
2022-07-20 17:01:57 +0530

    done

--- db.php
+++ db.php
@@ -5,6 +5,7 @@
     // Check connection
     $con=mysqli_connect('remotemysql.com','AJtedatBu3','ayYeXSJSOx','AJtedatBu3');
     if (mysqli_connect_errno()){
+        #neekendu ra
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 ?>
